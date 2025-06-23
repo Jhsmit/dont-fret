9c9bf52a8d3510a76fb839e39b4e6c494db65df2
Jochem Smit <jhsmit@gmail.com>
2025-06-23 12:59:47 +0200

    update web preload

--- templates/06_preload_web.py
+++ templates/06_preload_web.py
@@ -10,27 +10,31 @@ from pathlib import Path
 
 import solara
 
+from dont_fret.__main__ import load_config
+from dont_fret.web.methods import make_burst_nodes
 import dont_fret.web.state as state
 from dont_fret.web.main import Page as MainPage
-from dont_fret.web.methods import batch_burst_search
-from dont_fret.web.models import FRETNode, PhotonNode
+from dont_fret.web.models import FRETNode, ListStore, PhotonNode
+from dont_fret.config import cfg
+
+
+# %%
+# optionally change config
+load_config(Path("default_testing.yaml"))
 
 # %%
 
 ROOT = Path(__file__).parent.parent
 pth = ROOT / "tests" / "test_data" / "input" / "ds2"
-photon_file_items = [PhotonNode(file_path=ptu_pth) for ptu_pth in pth.glob("*.ptu")]
+photon_nodes = [PhotonNode(file_path=ptu_pth) for ptu_pth in pth.glob("*.ptu")]
+burst_nodes = make_burst_nodes(photon_nodes, cfg.burst_search)
+
 
 # %%
-
-burst_settings = ["DCBS", "APBS"]
-burst_items = [batch_burst_search(photon_file_items, name) for name in burst_settings]
-# %%
-# %%
-node = FRETNode(
-    name="FRET NOT",
-    photons=photon_file_items,
-    bursts=burst_items,
+node_1 = FRETNode(
+    name=solara.Reactive("my_node"),
+    photons=ListStore(photon_nodes),
+    bursts=ListStore(burst_nodes),
 )
 
 # %%
@@ -38,15 +42,21 @@ node = FRETNode(
 
 @solara.component
 def Page():
+    counter = solara.use_reactive(0)
+
     def preload():
         state.fret_nodes.set([])
-        if len(state.fret_nodes.value) == 0:
-            state.fret_nodes.extend([node])
+        if len(state.fret_nodes) == 0:
+            state.fret_nodes.extend([node_1])
+        state.disable_trace_page.set(False)
+        state.disable_trace_page.set(False)
 
-    solara.use_effect(preload, dependencies=[])
-    nodes = state.fret_nodes.value
+    solara.use_effect(preload, dependencies=[counter.value])
 
-    if len(nodes) != 0:
+    with solara.AppBar():
+        solara.Button("Reload", on_click=lambda: counter.set(counter.get() + 1))
+
+    if len(state.fret_nodes) != 0:
         MainPage()
     else:
         solara.Text("Loading fret nodes...")
